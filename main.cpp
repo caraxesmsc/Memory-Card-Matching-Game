ce990a4571b7d75682dad36976d40de9024457e6
caraxesmsc <mohamedongo21@gmail.com>
2023-11-28 17:52:23 +0200

    new main

--- main.cpp
+++ main.cpp
@@ -1,24 +1,38 @@
-#include "Queue.hpp"
-#include "Stack.hpp"
-#include "Card.hpp"
 
 #include <iostream>
-#include <ctime>
-#include <algorithm>
-
 using namespace std;
-int main() {
-    Queue myQueue;
+
+#include "Queue.hpp"
 
 
-    myQueue.createShuffledDeck();
+int main(int argc, const char* argv[]) {
+    Queue q1, card1, card2;
+    cout << "Queue created.  Empty? " << boolalpha << q1.empty() << endl;
+    q1.createShuffledQueue();
 
-    cout << "Shuffled Deck: ";
-    myQueue.printCards();
 
-    // Don't forget to free memory
-    myQueue.deleteQueue();
+    cout << "Contents of queue q1 (via  cout):\n";
+    cout << q1 << endl;
 
-    return 0;
+    cout << "Contents of queue q1 (via  printCards() ):\n";
+    q1.printCards();
 
+    bool gameMode = true;
+    while (gameMode) {
+        cout << "\nContents of queue q1 (via  displayGrid() ):\n\n";
+        q1.displayGrid();
+        int loc1 = q1.chooseCard();
+        int loc2 = q1.chooseCard();
+        bool checkFlag = q1.checkMatch(loc1, loc2, card1, card2);
+
+        if (checkFlag) {
+            cout << "Cards are matched." << endl;
+
+        }
+        else {
+            cout << "Cards are not matched." << endl;
+        }
+    }
+
+    q1.deleteQueue();
 }
